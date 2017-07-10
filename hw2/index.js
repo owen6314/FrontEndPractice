1ecc4f19c93c429c51606c2f2c890ac3e1f0c07c
owen <thss15_zhangqr@163.com>
2017-07-11 01:08:09 +0800

    hw2

--- hw2/index.js
+++ hw2/index.js
@@ -11,13 +11,49 @@ If you have any question, you can ask in our course wechat group.
 
 'use strict';
 
-function count() {
-  // TODO:
-  return null;
+function count(test) 
+{
+  var Counter=
+  {
+    number:0,
+    string:0,
+    undefined:0,
+    boolean:0,
+    count:function(src)
+    {
+      var a;
+      for(a in src)
+      {
+        if(src[a] instanceof Array)
+        {
+          this.count(src[a]);
+        }
+        else if(typeof(src[a]) == "number")
+        {
+          this.number++;
+        }
+        else if(typeof(src[a]) == "string")
+        {
+          this.string++;
+        }
+        else if(typeof(src[a]) == "boolean")
+        {
+          this.boolean++;
+        }
+        else if(typeof(src[a]) == "undefined")
+        {
+          this.undefined++;
+        }
+      }
+    }
+  }
+    var count_fun = Object.create(Counter);
+    count_fun.count(test); 
+    return null;
 }
 
-function isIPv4(s) {
-  // TODO:
+  function isIPv4(s) {
+    // TODO:
   return true;
 }
 
