ef782a8cbcdd8fbd33d35f13f367def66ec7b6dd
owen <thss15_zhangqr@163.com>
2017-07-11 08:20:42 +0800

    ipv4 finished

--- hw2/index.js
+++ hw2/index.js
@@ -47,13 +47,49 @@ function count(test)
       }
     }
   }
-    var count_fun = Object.create(Counter);
-    count_fun.count(test); 
-    return null;
+  var count_fun = Object.create(Counter);
+  count_fun.count(test); 
+  return count_fun;
 }
 
   function isIPv4(s) {
-    // TODO:
+  var list;
+  list = s.split('.');
+  //不能被解析为四部分
+  if(list.length != 4)
+    return false;
+  var a;
+  for(a in list)
+  {
+    //有空串
+    if(list[a]=='')
+    {
+      //console.info("match");
+      return false
+    }
+    var b;
+    //有字母
+    for(b in list[a])
+    {
+      if(isNaN(list[a][b]))
+      {
+        //console.info("list[a][b]");
+        return false;
+      }
+    }
+    //检测是否为合法的，且在0-256之间的数字
+    //最高位是0，长度不为1，错误
+    if(list[a][0]=='0' && list[a].length!=1)
+    {
+      return false;
+    }
+    //整数范围
+    var integer = parseInt(list[a]);
+    if(integer < 0 || integer > 255)
+    {
+      return false;
+    }
+  }
   return true;
 }
 
