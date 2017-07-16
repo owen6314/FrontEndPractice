f975553e16c54e4791fb3b9de730f66c324c3470
owen <thss15_zhangqr@163.com>
2017-07-17 07:36:13 +0800

    hw4 init

---
+++ hw4/js/main.js
@@ -0,0 +1,16 @@
+var BGCanvas;
+var BGContext;
+window.smove = {};
+smove.startGame = function()
+{
+	smove.init();
+}
+smove.init = function()
+{
+	BGCanvas = document.getElementById("background");
+	BGContext = BGCanvas.getContext("2d");
+	BGContext.fillStyle = "#00fe00";
+	BGContext.fillRect(100,100,100,100);
+}
+
+smove.startGame();
